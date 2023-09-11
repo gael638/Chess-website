975d61a2ba3f0840bc94359faa64959ab0d0eceb
gael638 <56929049+gael638@users.noreply.github.com>
2023-09-11 15:33:46 +0200

    Update functions.js

--- assets/scripts/functions.js
+++ assets/scripts/functions.js
@@ -78,3 +78,157 @@ function piece_move(last_case = "",new_case = "",piece_class = ""){
         }
     }
 }//takes classes (eg : .className) and move the selected piece
+
+
+
+function pawn_movement(row_class = [] , col_class = [] , row_value = 0 , col_value = 0 , enemy_color = "" , str = ""){
+    
+    let array = [];//<- to check if there is any pieces
+    switch (enemy_color){
+        case "fotsy" : 
+                        for(let i=col_value-1;i<=col_value+1;i++){
+                            array.push([piece_case(row_class[row_value+1]+" "+col_class[i]),i]);
+                        }
+                        if(str.includes("II") == true && str.includes("III") == false){
+                            for(let i=2;i<4;i++){
+                                if(piece_case(row_class[i]+" "+col_class[col_value]) == 1){
+                                    $(row_class[i]+" "+col_class[col_value]+" .selector").css("z-index","3");
+                                }else break;
+                            }
+                        }else if(array[1][0]==1){
+                                    $(row_class[row_value+1]+" "+col_class[array[1][1]]+" .selector").css("z-index","3");
+                                }
+                        if($(array[0][0]).hasClass("fotsy")){
+                            $(row_class[row_value+1]+" "+col_class[array[0][1]]+" .selector").css("z-index","3");
+                        }
+                        if($(array[2][0]).hasClass("fotsy")){
+                            $(row_class[row_value+1]+" "+col_class[array[2][1]]+" .selector").css("z-index","3");
+                        }
+                        break;
+
+        case "mainty" :  
+                        for(let i=col_value+1;i>=col_value-1;i--){
+                            array.push([piece_case(row_class[row_value-1]+" "+col_class[i]),i]);
+                        }
+                        if(str.includes("row.VII") == true){
+                            for(let i=5;i>3;i--){
+                                if(piece_case(row_class[i]+" "+col_class[col_value]) == 1){
+                                    $(row_class[i]+" "+col_class[col_value]+" .selector").css("z-index","3");
+                                }else break;
+                            }
+                        }else if(array[1][0]==1){
+                                $(row_class[row_value-1]+" "+col_class[array[1][1]]+" .selector").css("z-index","3");
+                        }
+                        if($(array[0][0]).hasClass("mainty")){
+                            $(row_class[row_value-1]+" "+col_class[array[0][1]]+" .selector").css("z-index","3");
+                        }
+                        if($(array[2][0]).hasClass("mainty")){
+                            $(row_class[row_value-1]+" "+col_class[array[2][1]]+" .selector").css("z-index","3");
+                        }
+    }                
+    
+
+}
+
+
+
+function rook_movement(row_class = [] , col_class = [] , row_value = 0 , col_value = 0 , enemy_color = "" , str = ""){
+    for(let i=row_value+1;i<8;i++){
+        if(piece_case(row_class[i]+" "+col_class[col_value]) == 1){
+            $(row_class[i]+" "+col_class[col_value]+" .selector").css("z-index","3");
+        }else{
+            if($(piece_case(row_class[i]+" "+col_class[col_value])).hasClass(enemy_color)){
+                $(row_class[i]+" "+col_class[col_value]+" .selector").css("z-index","3");
+            }
+            break;
+        }
+    }
+    for(let i=row_value-1;i>=0;i--){
+        if(piece_case(row_class[i]+" "+col_class[col_value]) == 1){
+            $(row_class[i]+" "+col_class[col_value]+" .selector").css("z-index","3");
+        }else{
+            if($(piece_case(row_class[i]+" "+col_class[col_value])).hasClass(enemy_color)){
+                $(row_class[i]+" "+col_class[col_value]+" .selector").css("z-index","3");
+            }
+            break;
+        }
+    }
+    for(let i=col_value+1;i<8;i++){
+        if(piece_case(row_class[row_value]+" "+col_class[i]) == 1){
+            $(row_class[row_value]+" "+col_class[i]+" .selector").css("z-index","3");
+        }else{
+            if($(piece_case(row_class[row_value]+" "+col_class[i])).hasClass(enemy_color)){
+                $(row_class[row_value]+" "+col_class[i]+" .selector").css("z-index","3");
+            }
+            break;
+        }
+    }
+    for(let i=col_value-1;i>=0;i--){
+        if(piece_case(row_class[row_value]+" "+col_class[i]) == 1){
+            $(row_class[row_value]+" "+col_class[i]+" .selector").css("z-index","3");
+        }else{
+            if($(piece_case(row_class[row_value]+" "+col_class[i])).hasClass(enemy_color)){
+                $(row_class[row_value]+" "+col_class[i]+" .selector").css("z-index","3");
+            }
+            break;
+        }
+    }
+}
+
+
+
+function bishop_movement(row_class = [] , col_class = [] , row_value = 0 , col_value = 0 , enemy_color = "" , str = ""){
+    for(let i=row_value+1,j=col_value+1;i<8;i++,j++){
+        if(piece_case(row_class[i]+" "+col_class[j]) == 1){
+            $(row_class[i]+" "+col_class[j]+" .selector").css("z-index","3");
+        }else{
+            if($(piece_case(row_class[i]+" "+col_class[j])).hasClass(enemy_color)){
+                $(row_class[i]+" "+col_class[j]+" .selector").css("z-index","3");
+            }
+            break;
+        }
+    }
+    for(let i=row_value-1,j=col_value-1;i>=0;i--,j--){
+        if(piece_case(row_class[i]+" "+col_class[j]) == 1){
+            $(row_class[i]+" "+col_class[j]+" .selector").css("z-index","3");
+        }else{
+            if($(piece_case(row_class[i]+" "+col_class[j])).hasClass(enemy_color)){
+                $(row_class[i]+" "+col_class[j]+" .selector").css("z-index","3");
+            }
+            break;
+        }    }
+    for(let i=row_value+1,j=col_value-1;i<8;i++,j--){
+        if(piece_case(row_class[i]+" "+col_class[j]) == 1){
+            $(row_class[i]+" "+col_class[j]+" .selector").css("z-index","3");
+        }else{
+            if($(piece_case(row_class[i]+" "+col_class[j])).hasClass(enemy_color)){
+                $(row_class[i]+" "+col_class[j]+" .selector").css("z-index","3");
+            }
+            break;
+        }    }
+    for(let i=row_value-1,j=col_value+1;i>=0;i--,j++){
+        if(piece_case(row_class[i]+" "+col_class[j]) == 1){
+            $(row_class[i]+" "+col_class[j]+" .selector").css("z-index","3");
+        }else{
+            if($(piece_case(row_class[i]+" "+col_class[j])).hasClass(enemy_color)){
+                $(row_class[i]+" "+col_class[j]+" .selector").css("z-index","3");
+            }
+            break;
+        }    }
+}
+
+
+
+function king_movement(row_class = [] , col_class = [] , row_value = 0 , col_value = 0 , enemy_color = "" , str = ""){
+    for(let i=col_value-1;i<col_value+2;i++){
+        for(let j=row_value-1;j<row_value+2;j++){
+            if(piece_case(row_class[j]+" "+col_class[i]) == 1){
+                $(row_class[j]+" "+col_class[i]+" .selector").css("z-index","3");
+            }else{
+                if($(piece_case(row_class[i]+" "+col_class[j])).hasClass(enemy_color)){
+                    $(row_class[i]+" "+col_class[j]+" .selector").css("z-index","3");
+                }
+            }
+        }
+    }
+}
